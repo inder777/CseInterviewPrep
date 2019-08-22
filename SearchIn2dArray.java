ed3318b0c13febcd13f52757126c69c357f915e1
Inderjeet Singh <interviewprep.cse@gmail.com>
2019-08-22 17:17:04 +0530

    Added Brute Force method

--- SearchIn2dArray.java
+++ SearchIn2dArray.java
@@ -18,11 +18,29 @@ public class Search {
             return false;
         }
 
+
+ public static boolean searchMatrixBruteForce(int[][] matrix, int x) {
+        if(matrix.length == 0)
+            return false;
+        int row = matrix.length;
+        int col = matrix[0].length;
+       for(int i=0;i<row;i++){
+           for(int j=0;j<col;j++){
+               if(matrix[i][j] == x)
+                   return true;
+           }
+       }
+        return false;
+    }
+
         public static void main(String[] s){
             int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
             System.out.println("==========Successful Search===========");
-            System.out.println("search result "+searchin2dMatrix(arr,6));
+            System.out.println("search result "+searchin2dMatrix(matrix,6));
+	    System.out.println("Brute Force Search "+searchMatrixBruteForce(matrix,6);
             System.out.println("==========Unsuccessful Search===========");
             System.out.println("search result "+searchin2dMatrix(arr,10));
+	    System.out.println("Brute Force Search "+searchMatrixBruteForce(matrix,16);
+
         }
     }
